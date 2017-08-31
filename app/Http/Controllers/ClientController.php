f08621a4f25424e9b8eb77a339d299d8d91c7e8b
Leandro Ribas <leanribas@gmail.com>
2017-08-31 19:01:05 -0300

    Criado CRUD para Model Client

---
+++ app/Http/Controllers/ClientController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace CodeProject\Http\Controllers;
+
+use CodeProject\Client;
+use Illuminate\Http\Request;
+use CodeProject\Http\Controllers\Controller;
+
+
+
+class ClientController extends Controller
+{
+
+    /**
+     * @var Client
+     */
+
+    protected $client;
+
+    /**
+     * ClientController constructor.
+     * @param Client $client
+     */
+    public function __construct(Client $client)
+    {
+        $this->client = $client;
+    }
+
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return $this->client->all();
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        return $this->client->create($request->all());
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        return $this->client->find($id);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $this->client->find($id)->update($request->all());
+        return $this->client->find($id);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $this->client->find($id)->delete();
+    }
+}
--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,6 +11,8 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('client','ClientController@index');
+Route::get('client/{id}', 'ClientController@show');
+Route::post('client', 'ClientController@store');
+Route::put('client/{id}', 'ClientController@update');
+Route::delete('client/{id}', 'ClientController@destroy');
